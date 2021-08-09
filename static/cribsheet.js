d3a53a81baaa060d8aca18e9cf67fd817b87eacc
ckollett <ckollett@gmail.com>
2021-08-09 13:02:56 -0400

    Link to next empty row in crib sheet

--- static/cribsheet.js
+++ static/cribsheet.js
@@ -1,7 +1,7 @@
 var lastGameWinner;
 
 function getLastScore() {
-  const url = "https://sheets.googleapis.com/v4/spreadsheets/1gCBMVv-61otx1OEZQKz4_sPqXpFQSbWcJC6Vx1FlfhE/values/Results!A:C?key=AIzaSyDOUQdHsH-KQZje1ipkNGowjA_fvXfipak&majorDimension=COLUMNS";
+  const url = "https://sheets.googleapis.com/v4/spreadsheets/1gCBMVv-61otx1OEZQKz4_sPqXpFQSbWcJC6Vx1FlfhE/values/Latest!A:C?key=AIzaSyDOUQdHsH-KQZje1ipkNGowjA_fvXfipak&majorDimension=COLUMNS";
   var xhr = new XMLHttpRequest();
   xhr.responseType = "json";
   xhr.onreadystatechange = function() {
@@ -19,4 +19,10 @@ function showLastScore(scoreData) {
     lastGameWinner = winner[winner.length-1];
     const msg = "Last game winner: " + lastGameWinner;
     document.getElementById('lastscore').innerHTML = msg;
+    
+    // Update the crib sheet link to go to the first empty row.
+    const sheetLink = document.getElementById('cribsheetlink');
+    var link = sheetLink.getAttribute('href');
+    link += '&range=A' + (winner.length+1);
+    sheetLink.setAttribute('href', link);
 }
